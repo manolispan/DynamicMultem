f1eaf612d047445be6584b1eedee7054a1c4ee8f
manolispan <manolispan1988@gmail.com>
2022-11-27 03:44:00 +0200

    fixed many graphs

--- pages/plot.js
+++ pages/plot.js
@@ -4,11 +4,11 @@ import Axios from "axios";
 import classes from "./plot.module.css";
 import ConfirmPrompt from "../components/ui/confirmprompt/confirmprompt"
 
-function Plots() {
+function Plots(props) {
   const [savePrompt, setSavePrompt] = useState(false);
 
   const [width, setWidth] = useState(800);
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [xAxis, setXAxis] = useState([]);
   const [yAxis, setYAxis] = useState([]);
@@ -23,7 +23,15 @@ function Plots() {
   const outputTypeRef= useRef();
 
   const [graphColor,setGraphColor]=useState("blue");
-  const AllColors =["blue","black","red","yellow"]
+  const AllColors =["blue","black","red","yellow"];
+
+  const [plotData,setPlotData] =useState([]);
+
+  const [numberofPlots,setNumberOfPlots]=useState([0,1,2])
+
+  let idextra= "";
+  if (props.idextra) {idextra=pros.idextra};
+ 
 
   useEffect(() => {
     let outputs = {};
@@ -47,10 +55,12 @@ function Plots() {
         adiabScatn : adiabnOut.data
       }
 
-
-      setData(staticOut.data);
+      /* let tempdata=data.slice(0);
+      tempdata[0]={};
+      tempdata[0]=staticOut.data
+      setData(tempdata);
       setXAxis(staticOut.data.freq);
-      setYAxis(staticOut.data.trans);
+      setYAxis(staticOut.data.trans); */
 
       setLoadedResultFilesList(["staticScat","dynScat","adiabScat","staticScatn","dynScatn","adiabScatn"])
       setLoadedResults(outputs);
@@ -136,7 +146,6 @@ function Plots() {
   
 
 
-
   const Plot = dynamic(import('react-plotly.js'), {
     ssr: false
   })
@@ -180,13 +189,25 @@ function Plots() {
         />
       }
   
+<div className={classes.alldata}>
 
-
+  {
+    numberofPlots.map((number)=>
+    <div>
 <div className={classes.savedfiles}> 
 
   <div>File
-    <select onChange={(e) => { setData(loadedResults[e.target.value]) }
+    <select onChange={(e) => { 
+            let tempdata=data.slice(0);
+            if (typeof tempdata[number] != 'object' ) {tempdata[number]={}};
+            tempdata[number]=loadedResults[e.target.value]
+            setData(tempdata);
+            document.getElementById("xaxis"+number+idextra).selectedIndex = 0;
+            document.getElementById("yaxis"+number+idextra).selectedIndex = 0;
+
+      }
         }>
+          <option disabled selected>Select File</option>
           {loadedResultFilesList.map((keyName, i) => (
             <option key={i+"tt"} value={keyName}>
               {keyName}
@@ -256,12 +277,18 @@ function Plots() {
 
 </div>
 
-
-
-      <div> Y AXIS
-        <select onChange={(e) => { setYAxis(data[e.target.value]) }
+<div className={classes.settings}>
+        <div> Y AXIS
+        <select 
+        id = {"yaxis"+number+idextra}
+        onChange={(e) => { 
+          let temp = plotData.slice(0);
+          if (typeof temp[number] != 'object' ) {temp[number]={}};
+          temp[number].y = data[number][e.target.value]
+          setPlotData(temp) }
         }>
-          {Object.keys(data).map((keyName, i) => (
+           <option disabled selected>Select Y</option>
+          {data[number] && Object.keys(data[number]).map((keyName, i) => (
             <option key={i} value={keyName}>
               {keyName}
             </option>
@@ -272,9 +299,19 @@ function Plots() {
       </div>
 
       <div> X AXIS
-        <select onChange={(e) => { setXAxis(data[e.target.value]) }
-        }>
-          {Object.keys(data).map((keyName, i) => (
+        <select 
+        id = {"xaxis"+number+idextra}
+          onChange={(e) => { 
+           let temp = plotData.slice(0);
+           if (typeof temp[number] != 'object' ) {temp[number]={}};
+          temp[number].x = data[number][e.target.value]
+          temp[number].type= 'scatter'
+          temp[number].mode= 'lines'
+          setPlotData(temp)
+         }
+        }> 
+        <option disabled selected>Select X</option>
+          {data[number] &&  Object.keys(data[number]).map((keyName, i) => (
             <option key={i+"x"} value={keyName}>
               {keyName}
             </option>
@@ -286,7 +323,13 @@ function Plots() {
 
 
       <div> Color
-        <select onChange={(e) => { setGraphColor(e.target.value) }
+        <select onChange={(e) => { 
+           let temp = plotData.slice(0);
+           if (typeof temp[number] != 'object' ) {temp[number]={}};
+           if (!temp[number].marker) {temp[number].marker={}}
+          temp[number].marker.color = e.target.value
+          setPlotData(temp)
+         }
         }>
           {AllColors.map((i) => (
             <option key={i+"x"} value={i}>
@@ -297,34 +340,335 @@ function Plots() {
         </select>
 
       </div>
+</div>
+
+</div>
+    )
+  }
+
+{/* <div>
+<div className={classes.savedfiles}> 
+
+  <div>File
+    <select onChange={(e) => { 
+            let tempdata=data.slice(0);
+            if (typeof tempdata[0] != 'object' ) {tempdata[0]={}};
+            tempdata[0]=loadedResults[e.target.value]
+            setData(tempdata);
+            document.getElementById("xaxis0").selectedIndex = 0;
+            document.getElementById("yaxis0").selectedIndex = 0;
+
+      }
+        }>
+          <option disabled selected>Select File</option>
+          {loadedResultFilesList.map((keyName, i) => (
+            <option key={i+"tt"} value={keyName}>
+              {keyName}
+            </option>
+          ))}
+
+        </select></div>
+
+<div style={{position: "relative"}}><button onClick={()=>setSavedFilesPrompt(true)}>Saved Files</button>
+
+{savedFilesPrompt &&
+      <ConfirmPrompt
+      text = {<div>
+      <h4>Which file to load?</h4>
+      <div>Static</div>
+      <div className={classes.inputlist}>
+        {savedResults.static.map((name)=>
+        <div
+        key={name}
+        id={name}
+        onClick={(e)=> {setChosenOutput(e.target.id);
+        outputTypeRef.current="savedstatic"
+       
+      }}
+        className ={chosenOutput==name ? classes.inputlistchosen : null}
+        >{name}</div>
+        )}
+      </div>
+
+      <div>Dynamic</div>
+      <div className={classes.inputlist}>
+        {savedResults.dyn.map((name)=>
+        <div
+        key={name}
+        id={name}
+        onClick={(e)=> {setChosenOutput(e.target.id);
+          outputTypeRef.current="saveddyn"
+        
+        }}
+        className ={chosenOutput==name ? classes.inputlistchosen : null}
+        >{name}</div>
+        )}
+      </div>
+
+      <div>Adiabatic</div>
+      <div className={classes.inputlist}>
+        {savedResults.adiab.map((name)=>
+        <div
+        key={name}
+        id={name}
+        onClick={(e)=> {setChosenOutput(e.target.id);
+          outputTypeRef.current="savedadiab"
+        }}
+        className ={chosenOutput==name ? classes.inputlistchosen : null}
+        >{name}</div>
+        )}
+      </div>
+      </div>
+        }
+        cancel ={()=>setSavedFilesPrompt(false)}
+        notext = "Cancel"
+        yestext ="Ok"
+        ok = {LoadOutputHandler}
+      />
+      }
+</div>
+
+</div>
+
+<div className={classes.settings}>
+        <div> Y AXIS
+        <select 
+        id = "yaxis0"
+        onChange={(e) => { 
+          let temp = plotData.slice(0);
+          if (typeof temp[0] != 'object' ) {temp[0]={}};
+          temp[0].y = data[0][e.target.value]
+          setPlotData(temp) }
+        }>
+           <option disabled selected>Select Y</option>
+          {data[0] && Object.keys(data[0]).map((keyName, i) => (
+            <option key={i} value={keyName}>
+              {keyName}
+            </option>
+          ))}
+
+        </select>
+
+      </div>
+
+      <div> X AXIS
+        <select 
+        id = "xaxis0"
+          onChange={(e) => { 
+           let temp = plotData.slice(0);
+           if (typeof temp[0] != 'object' ) {temp[0]={}};
+          temp[0].x = data[0][e.target.value]
+          temp[0].type= 'scatter'
+          temp[0].mode= 'lines'
+          setPlotData(temp)
+         }
+        }> 
+        <option disabled selected>Select X</option>
+          {data[0] &&  Object.keys(data[0]).map((keyName, i) => (
+            <option key={i+"x"} value={keyName}>
+              {keyName}
+            </option>
+          ))}
+
+        </select>
+
+      </div>
 
 
+      <div> Color
+        <select onChange={(e) => { 
+           let temp = plotData.slice(0);
+           if (typeof temp[0] != 'object' ) {temp[0]={}};
+           if (!temp[0].marker) {temp[0].marker={}}
+          temp[0].marker.color = e.target.value
+          setPlotData(temp)
+         }
+        }>
+          {AllColors.map((i) => (
+            <option key={i+"x"} value={i}>
+              {i}
+            </option>
+          ))}
+
+        </select>
+
+      </div>
+</div>
+
+</div>
+
+<div>
+<div className={classes.savedfiles}> 
+
+  <div>File
+    <select onChange={(e) => { 
+                  let tempdata=data.slice(0);
+                  if (typeof tempdata[1] != 'object' ) {tempdata[1]={}};
+                  tempdata[1]=loadedResults[e.target.value]
+                  setData(tempdata)
+                  document.getElementById("xaxis1").selectedIndex = 0;
+                  document.getElementById("yaxis1").selectedIndex = 0;
+                  ;
+                  
+     }
+        }>
+           <option disabled selected>Select File</option>
+          {loadedResultFilesList.map((keyName, i) => (
+            <option key={i+"tt"} value={keyName}>
+              {keyName}
+            </option>
+          ))}
+
+        </select></div>
+
+<div style={{position: "relative"}}><button onClick={()=>setSavedFilesPrompt(true)}>Saved Files</button>
+
+{savedFilesPrompt &&
+      <ConfirmPrompt
+      text = {<div>
+      <h4>Which file to load?</h4>
+      <div>Static</div>
+      <div className={classes.inputlist}>
+        {savedResults.static.map((name)=>
+        <div
+        key={name}
+        id={name}
+        onClick={(e)=> {setChosenOutput(e.target.id);
+        outputTypeRef.current="savedstatic"
+       
+      }}
+        className ={chosenOutput==name ? classes.inputlistchosen : null}
+        >{name}</div>
+        )}
+      </div>
+
+      <div>Dynamic</div>
+      <div className={classes.inputlist}>
+        {savedResults.dyn.map((name)=>
+        <div
+        key={name}
+        id={name}
+        onClick={(e)=> {setChosenOutput(e.target.id);
+          outputTypeRef.current="saveddyn"
+        
+        }}
+        className ={chosenOutput==name ? classes.inputlistchosen : null}
+        >{name}</div>
+        )}
+      </div>
+
+      <div>Adiabatic</div>
+      <div className={classes.inputlist}>
+        {savedResults.adiab.map((name)=>
+        <div
+        key={name}
+        id={name}
+        onClick={(e)=> {setChosenOutput(e.target.id);
+          outputTypeRef.current="savedadiab"
+        }}
+        className ={chosenOutput==name ? classes.inputlistchosen : null}
+        >{name}</div>
+        )}
+      </div>
+      </div>
+        }
+        cancel ={()=>setSavedFilesPrompt(false)}
+        notext = "Cancel"
+        yestext ="Ok"
+        ok = {LoadOutputHandler}
+      />
+      }
+</div>
+
+</div>
+
+<div className={classes.settings}>
+        <div> Y AXIS
+        <select 
+          id = "yaxis1"
+          onChange={(e) => { 
+          let temp = plotData.slice(0);
+          if (typeof temp[1] != 'object' ) {temp[1]={}};
+          temp[1].y = data[1][e.target.value]
+          setPlotData(temp) }
+        }>
+          <option disabled selected>Select Y</option>
+          {data[1] &&  Object.keys(data[1]).map((keyName, i) => (
+            <option key={i} value={keyName}>
+              {keyName}
+            </option>
+          ))}
+
+        </select>
+
+      </div>
+
+      <div> X AXIS
+        <select 
+        id = "xaxis1"
+        onChange={(e) => { 
+           let temp = plotData.slice(0);
+           if (typeof temp[1] != 'object' ) {temp[1]={}};
+          temp[1].x = data[1][e.target.value]
+          temp[1].type= 'scatter'
+          temp[1].mode= 'lines'
+          setPlotData(temp)
+         }
+        }>
+          <option disabled selected>Select X</option>
+          {data[1] &&  Object.keys(data[1]).map((keyName, i) => (
+            <option key={i+"x"} value={keyName}>
+              {keyName}
+            </option>
+          ))}
+
+        </select>
+
+      </div>
+
+
+      <div> Color
+        <select onChange={(e) => { 
+           let temp = plotData.slice(0);
+           if (typeof temp[1] != 'object' ) {temp[1]={}};
+           if (!temp[1].marker) {temp[1].marker={}}
+          temp[1].marker.color = e.target.value
+          setPlotData(temp)
+         }
+        }>
+          {AllColors.map((i) => (
+            <option key={i+"x"} value={i}>
+              {i}
+            </option>
+          ))}
+
+        </select>
+
+      </div>
+</div>
+
+</div> */}
+
+
+<div className={classes.addgraph}><div
+onClick={()=>
+{let temp=numberofPlots.slice(0);
+  temp.push(temp[temp.length-1]+1);
+  setNumberOfPlots(temp)
+
+}
+}
+>Add graph</div></div>
+</div>
       <div className={classes.savePrompt}>
         <div onClick={() => setSavePrompt(true)}>Save Input-Results</div>
 
       </div>
 
       <Plot
-        data={[
-          {
-            x: xAxis,
-            y: yAxis,
-            type: 'scatter',
-            mode: 'lines',
-            marker: { color: graphColor },
-
-          },
-          /*         {
-                      x: xAxis,
-                      y: data.ref,
-                      type: 'scatter',
-                      mode: 'lines+markers',
-                      marker: {color: 'blue'},
-                    }, */
-
-        ]}
+        data = {plotData}
         layout={{ width: width, height: 600, title: 'Best Plots Here' }}
-        config={{ scrollZoom: true }} />
+        config={{ scrollZoom: true, editable: true } } />
     </>}
 
 
