685bc4fcde5e64f183f431c2118c2627327a9187
manolispan <manolispan1988@gmail.com>
2023-07-13 00:03:49 +0300

    hope it works

--- pages/plot.js
+++ pages/plot.js
@@ -438,7 +438,28 @@ setIsLoading(false);
         config={{ scrollZoom: true, editable: true }} />
     </>}
 
+{/*     <Plot
+        data={[{
 
+          z: [[10, 10.625, 12.5, 15.625, 20],
+        
+               [5.625, 6.25, 8.125, 11.25, 15.625],
+        
+               [2.5, 3.125, 5., 8.125, 12.5],
+        
+               [0.625, 1.25, 3.125, 6.25, 10.625],
+        
+               [0, 0.625, 2.5, 5.625, 10]],
+        
+          x: [-9, -6, -5 , -3, -1],
+        
+          y: [0, 1, 4, 5, 7],
+        
+          type: 'contour'
+        
+        }]}
+        layout={{ width: width, height: 600, title: 'Best Plots Here' }}
+        config={{ scrollZoom: true, editable: true }} /> */}
   </div>
 }
 
